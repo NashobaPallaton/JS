2b899bac0970f4dc1589471f907219940e6bb68f
Nashoba <gastout.camille@gmail.com>
2022-06-23 16:20:14 +0200

    buletin OK

---
+++ exo_form_multiplication/script.js
@@ -0,0 +1,7 @@
+function checkForm() {
+    a = document.getElementById("a").value;
+    b = document.getElementById("b").value;
+    alert(a * b);
+
+        return false;
+}
--- note/script.js
+++ note/script.js
@@ -1,59 +1,21 @@
-            
-    let math = document.getElementByID("inputnote1").value;
-    let fr = document.getElementByID("inputnote2").value;
-    let hg = document.getElementByID("inputnote3").value;
-    let sport = document.document.getElementByID("inputnote4").value;
-    let ap = document.getElementByID("inputnote5").value;
-    let pc = document.getElementByID("inputnote6").value; 
+function calculer_moyenne() {
 
+  math = document.getElementById("inputnote1").value;
+  fr = document.getElementById("inputnote2").value;
+  hg = document.getElementById("inputnote3").value;
+  sport = document.getElementById("inputnote4").value;
+  ap = document.getElementById("inputnote5").value;
+  pc = document.getElementById("inputnote6").value;
 
+  math = parseInt(math);
+  fr = parseInt(fr);
+  hg = parseInt(hg);
+  sport = parseInt(sport);
+  ap = parseInt(ap);
+  pc = parseInt(pc);
 
-function get_value(){
-         if (isNaN(input) == true) {
-       alert('Merci de saisir un montant correct. Calcul impossible.');
-         Form1.ht.value = 0 ;     } else {
-var input = document.getElementsByName("inputnote1").value ;
-alert(input); }
-    
+  moy = (math + fr + hg + sport + ap + pc) / 6;
+
+  alert(moy.toFixed(2))
+
 }
-
-
-
-function calculer_moyenne(){
-
-    moy = (math+fr+hg+sport+ap+pc)/6;
-    
-}
-
-// const btn = document.querySelector('button');
-// const txt = document.querySelector('p');
-
-// btn.addEventListener('click', updateBtn);
-
-// function updateBtn() {
-//     console.log('clic')
-//     if (btn.textContent === 'Démarrer la machine') {
-//     btn.textContent = 'Arrêter la machine';
-//     txt.textContent = 'La machine est en marche&nbsp;!';
-//   } else {
-//     btn.textContent = 'Démarrer la machine';
-//     txt.textContent = 'La machine est arrêtée.';
-//   }
-// }
-
-
-
-//doc formulaire
-
-// function CalculerMontantTTC() {
-//     if (isNaN(Form1.ht.value) == true) {
-//         alert('Merci de saisir un montant correct. Calcul impossible.');
-//         Form1.ht.value = 0 ;
-//     } else {
-//         //form1 c'est le nom de mon formulaire Ht le nom de mon input et value est la valeur dans cette input
-//         Form1.tva.value = (Form1.boisson.value / 100) * 19.6; //calcul de tva
-//         Form1.ttc.value = Number(Form1.boisson.value) + Number(Form1.tva.value);// prix ttc
-//         Form1.prixboissons.value = Number(Form1.boisson.value);
-
-//     }
-// }
