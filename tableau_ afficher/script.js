37a4331630cb42cebf9bed25bc0f3f2da90e4903
NashobaPallaton <gastout.camille@gmail.com>
2022-07-07 14:10:34 +0200

    exercice 07/07

---
+++ tableau_ afficher/script.js
@@ -0,0 +1,14 @@
+let student = ['Salomé ', 'Océane ', 'Karim ', 'Jimmy ', 'Ben ', 'Fred ', 'Greg ', 'Cam ', 'Antho ', 'Cédric ', 'Steph ', 'Perrine ', 'Théo ', 'Outman ', 'Halim ', 'Zak ']
+
+
+for (let i=0; i<student.length; i++){
+    document.write(student[i]);
+    
+    if (i==10) {
+        break;
+    }
+
+}
+
+
+
