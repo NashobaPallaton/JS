3336d9f32dc7ccb8bc7f8bff4e9b877cc0a88a98
NashobaPallaton <gastout.camille@gmail.com>
2022-07-07 16:39:37 +0200

    presque fini

--- tableau_ afficher/script.js
+++ tableau_ afficher/script.js
@@ -1,13 +1,7 @@
-let student = ['Salomé ', 'Océane ', 'Karim ', 'Jimmy ', 'Ben ', 'Fred ', 'Greg ', 'Cam ', 'Antho ', 'Cédric ', 'Steph ', 'Perrine ', 'Théo ', 'Outman ', 'Halim ', 'Zak ']
+let student = ['Salomé', 'Océane', 'Karim', 'Jimmy', 'Ben', 'Fred', 'Greg', 'Cam', 'Antho', 'Cédric', 'Steph', 'Perrine', 'Théo', 'Outman', 'Halim', 'Zak']
 
-
-for (let i=0; i<student.length; i++){
-    document.write(student[i]);
-    
-    if (i==10) {
-        break;
-    }
-
+function recive(){
+    let nom = document.getElementById("entry").value;
 }
 
 
