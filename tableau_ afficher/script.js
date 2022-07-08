8d28ac2c9cfae0c1a6eeba6606891f3325d5c830
NashobaPallaton <gastout.camille@gmail.com>
2022-07-08 09:09:59 +0200

    backtick

--- tableau_ afficher/script.js
+++ tableau_ afficher/script.js
@@ -1,7 +1,9 @@
-let student = ['Salomé', 'Océane', 'Karim', 'Jimmy', 'Ben', 'Fred', 'Greg', 'Cam', 'Antho', 'Cédric', 'Steph', 'Perrine', 'Théo', 'Outman', 'Halim', 'Zak']
+let student = ['Salomé', 'Océane', 'Karim', 'Jimmy', 'Ben', 'Fred', 'Greg', 'Cam', 'Antho', 'Cédric', 'Steph', 'Perrine', 'Théo', 'Outman', 'Halim', 'Zak'];
 
 function recive(){
     let nom = document.getElementById("entry").value;
+
+    console.log(`le nom entré est ${nom}`)
 }
 
 
