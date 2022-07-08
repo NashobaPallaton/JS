9fb97267478f2177bd09b6c805a9417f04f60c80
NashobaPallaton <gastout.camille@gmail.com>
2022-07-08 10:59:45 +0200

    modif

--- tableau_ afficher/script.js
+++ tableau_ afficher/script.js
@@ -5,24 +5,24 @@ console.log(`le diminutif de Benoît est : ${student[4]}`)
 
 //methode push() pour ajouter des élément à la suite dans le tableau
 student.push("Zaza", "Mimi")
-console.log(`methode push ${student}`)
+console.log(`methode push() ${student}`)
 
 //methode shift() pour retirer le premier élément du tableau
 student.shift()
-console.log(`methode shift ${student}`)
+console.log(`methode shift() ${student}`)
 //permets aussi d'afficher le premier élément ici Océane vu que Salomé shifté au dessus
 firststud = student.shift()
-console.log(`methode shift ${firststud}`)
+console.log(`methode shift() ${firststud}`)
 
 
 //methode concat() pour lier des tableaux
 let list = student.concat(student2);
-console.log(`methode concat ${list}`)
+console.log(`methode concat() ${list}`)
 
 //methode splice() pour ajouter des elements au tableau à une position donnée
 //ici ajoute Toto en quatrième position 
 student2.splice(3, 0, "Toto")
-console.log(`methode splice ${student2}`)
+console.log(`methode splice() ${student2}`)
 
 //methode pop() retire le dernier élément du tableau
 student2.pop()
@@ -33,7 +33,7 @@ console.log(`methode pop() ${laststud}`)
 
 //methode copyWithin() copie des éléments du tableaux par dessus des element du dit tableau
 //ici copy l'élément 0 en position 2
-console.log(`methode copyWithin ${student2.copyWithin(2, 0)}`)
+console.log(`methode copyWithin() ${student2.copyWithin(2, 0)}`)
 
 //methode fill() remplace les éléments du tableau par le nouvel élément
 console.log(`methode fill() ${student2.fill("Toto")}`)
