2cf579cbbf64becc92981fb9965d268ad042218f
NashobaPallaton <gastout.camille@gmail.com>
2022-07-11 11:08:40 +0200

    carré comme un kiri

--- Exercice 11-07/script.js
+++ Exercice 11-07/script.js
@@ -1,7 +1,8 @@
 function recive(){
     let number = document.getElementById("entry").value;
 
-    document.getElementById("response").innerHTML = `Le carré de ${number} est ${number*number}`;
+    document.getElementById("response").innerHTML = `Le carré de ${number} est ${number*number}`; //backtick
+    document.getElementById("response2").innerHTML = "Le carré de " + number + " est " + number*number; //vieille methode
+
     console.log(`Le carré de ${number} est ${number*number}`);
 }
-
