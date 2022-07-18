47ea9533fa891b15ee32042127797088e53aead6
NashobaPallaton <gastout.camille@gmail.com>
2022-07-18 14:14:38 +0200

    to-do list

--- Nombre_premier/script.js
+++ Nombre_premier/script.js
@@ -3,13 +3,14 @@ function premier(){
     n = parseInt(n);
 
 if(n<2)
-document.write(`${n} est pas  nombre premier`)
+document.write(`${n} n'est pas  nombre premier`)
 
 
-for (let i = 2; i<n; i++)
+for (let i = 2; i < n; i++)
     if (n % i == 0)
     document.write(`${n} n'est pas  nombre premier`)
     
     else
         document.write(`${n} est un nombre premier`)
+    
 }
---
+++ Todo_list/script.js
@@ -0,0 +1,27 @@
+function add(){
+    let newItem = document.createElement("div");
+    newItem.innerHTML = document.getElementById("box").value;
+    newItem.onclick = remove;
+    document.getElementById("list").appendChild(newItem);
+    saveList();
+}
+
+function remove(){
+    document.getElementById("list").removeChild(this);
+    saveList();
+}
+
+function saveList(){
+    localStorage.storedList = document.getElementById("list").innerHTML;
+}
+
+function load(){
+    document.getElementById("list").innerHTML = localStorage.storedList;
+    for(var i=0; i < list.children.lenght; i++){
+        list.children[1].onclick = remove;
+    }
+}
+
+if(localStorage.storedList){
+    load();
+}
