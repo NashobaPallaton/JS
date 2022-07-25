d1547e0d282edb57af1df4f5e9f55f5008341139
NashobaPallaton <gastout.camille@gmail.com>
2022-07-25 16:21:24 +0200

    1st try

---
+++ exercie_regex_25-07/script.js
@@ -0,0 +1,20 @@
+let form = document.querySelector('#loginForm');
+
+form.email.addEventListener('change', function(){
+    validEmail(this);
+});
+
+const validEmail = function(mail) {
+    let emailRegexp = new RegExp(
+        '^[a-zA-Z0-9.-_]+@{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+
+        let testemail = emailRegexp.test(mail.value);
+        let small = mail.nextElementSibling;
+
+    if (testemail) {
+        small.innerHTML = 'Adresse Valide';
+    }    
+    else{
+        small.innerHTML = 'Adresse Invalide'
+    }
+}
