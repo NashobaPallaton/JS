3598abafdc122eebd1e66a831b41d4620f47e43c
NashobaPallaton <gastout.camille@gmail.com>
2022-07-27 14:36:42 +0200

    btn fonctionnel

--- Popup/script.js
+++ Popup/script.js
@@ -3,10 +3,14 @@ window.addEventListener("load", ()=>{
     function open(event){
        document.querySelector(".popup").style.display = "block";
     }, 
-   2000)
+   1500)
 });
 
 
 document.querySelector("#close").addEventListener("click", ()=>{
+    document.querySelector(".popup").style.display = "none";
+});
+
+document.querySelector("a").addEventListener("click", ()=>{
     document.querySelector(".popup").style.display = "none";
 });
